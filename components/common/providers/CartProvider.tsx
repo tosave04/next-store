3d2cab08ceb8ebd7928c4075767b9e70935396e7
Thomas Savournin <tosave.vbl@gmail.com>
2022-12-21 08:41:48 +0100

    build: structure dossiers

---
+++ components/common/providers/CartProvider.tsx
@@ -0,0 +1,3 @@
+export default function CartProvider({ children }: { children: React.ReactNode }) {
+	return children
+}
---
+++ components/layout/index.tsx
@@ -0,0 +1,5 @@
+export { default as Footer } from "./Footer"
+export { default as Header } from "./Header"
+export { default as Logo } from "./Logo"
+export { default as Nav } from "./Nav"
+export { default as Sidebar } from "./Sidebar"
