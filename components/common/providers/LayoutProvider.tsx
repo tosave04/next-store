eb3f8701412ef90a70959081410e036293304dd7
Thomas Savournin <tosave.vbl@gmail.com>
2022-12-22 10:28:26 +0100

    feat: LayoutProvider

--- components/common/providers/LayoutProvider.tsx
+++ components/common/providers/LayoutProvider.tsx
@@ -1,3 +1,17 @@
+import React from "react"
+import useDarkMode from "../hooks/useDarkMode"
+import type { DarkModeType } from "../hooks/useDarkMode"
+
+export const LayoutContext = React.createContext<LayoutType>({} as LayoutType)
+
 export default function LayoutProvider({ children }: { children: React.ReactNode }) {
-	return children
+	// Dark Mode
+	const { darkMode, toogleDarkMode } = useDarkMode()
+
+	return <LayoutContext.Provider value={{ darkMode, toogleDarkMode }}>{children}</LayoutContext.Provider>
+}
+
+type LayoutType = {
+	darkMode: DarkModeType["darkMode"]
+	toogleDarkMode: DarkModeType["toogleDarkMode"]
 }
