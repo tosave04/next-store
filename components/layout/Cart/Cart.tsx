494ace0a08354b5204eb33e3b5c0e376cac88f9c
Thomas Savournin <tosave.vbl@gmail.com>
2022-12-22 15:26:45 +0100

    feat: Sidebar & Cart

---
+++ components/layout/Cart/Cart.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react"
+import { LayoutContext } from "../../common/providers/LayoutProvider"
+
+export default function Cart() {
+	const { cart } = React.useContext(LayoutContext)
+
+	const expanded = "w-72 right-0"
+	const hidden = "w-0 -right-72"
+
+	return (
+		<aside
+			className={`fixed flex flex-col gap-2 h-1/2 p-4 py-8 bg-white border-r dark:bg-gray-900 dark:border-gray-700 shadow-lg transition-all ${
+				cart ? expanded : hidden
+			}`}
+		>
+			<h2 className="text-3xl font-semibold text-gray-800 dark:text-white m-0">Panier</h2>
+			<hr className="my-4 border-gray-200 dark:border-gray-600" />
+		</aside>
+	)
+}
